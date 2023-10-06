eb75cbe6664dfcd8075076bcff9ff6ca04beab3a
Itsfelps <128159625+Itsfelps@users.noreply.github.com>
2023-10-06 12:00:49 -0300

    Add files via upload

---
+++ pedir.js
@@ -0,0 +1,29 @@
+function pedir(){
+    var opcao = prompt("Escolha uma opçao de 1 a 5")
+
+ switch(Number(opcao)){
+case 1:
+        alert("Voce escolheu Janeiro")
+        break;  
+        
+case 2:
+        alert("Voce escolheu Fevereiro")
+        break; 
+
+case 3 :
+      alert("Voce escolheu Março")
+      break;
+    
+case 4 :
+       alert("Voce escolheu Abril") 
+       break;
+    
+case 5 :
+     alert("Voce escolheu Maio")
+     break;
+
+    }
+
+}
+
+
